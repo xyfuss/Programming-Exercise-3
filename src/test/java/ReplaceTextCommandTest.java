28847422661603e8fadbd48cf8a590bca4583324
xyfuss <jonasaatomren@gmail.com>
2025-02-10 14:54:53 +0100

    created test for execute method

--- src/test/java/ReplaceTextCommandTest.java
+++ src/test/java/ReplaceTextCommandTest.java
@@ -1,9 +1,12 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class ReplaceTextCommandTest {
 
-  @org.junit.jupiter.api.Test
-  void execute() {
 
+  @org.junit.jupiter.api.Test
+  void testExecutePositive() {
+    ReplaceTextCommand test1 = new ReplaceTextCommand("target","replacement");
+    assertEquals("text with replacement and replacement",test1.execute("text with target and target"));
   }
 }
