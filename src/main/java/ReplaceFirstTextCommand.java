7082fc07c6415143aa8b99d4ec42cc2bba8e83d9
xyfuss <jonasaatomren@gmail.com>
2025-02-10 14:59:21 +0100

    added methods for ReplaceFirstTextCommand class

--- src/main/java/ReplaceFirstTextCommand.java
+++ src/main/java/ReplaceFirstTextCommand.java
@@ -1,2 +1,34 @@
-public class ReplaceFirstTextCommand {
+public class ReplaceFirstTextCommand implements TextCommand{
+  private String target;
+  private String replacement;
+
+  public ReplaceFirstTextCommand(String target, String replacement) {
+    this.setTarget(target);
+    this.setReplacement(replacement);
+  }
+  public void setTarget(String target) {
+    if (target == null || target.isBlank() || target.isEmpty()) {
+      throw new IllegalArgumentException();
+    }
+    this.target = target;
+  }
+
+  public void setReplacement(String replacement) {
+    if (replacement == null || replacement.isBlank() || replacement.isEmpty()) {
+      throw new IllegalArgumentException();
+    }
+    this.replacement = replacement;
+  }
+
+  public String getTarget() {
+    return target;
+  }
+
+  public String getReplacement() {
+    return replacement;
+  }
+
+  public String execute(String text) {
+    return text.replaceFirst(this.getTarget(),this.getReplacement());
+  }
 }
