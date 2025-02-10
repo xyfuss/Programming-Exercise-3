6265d67998c0e6045063f4b9253953d69db2a936
xyfuss <jonasaatomren@gmail.com>
2025-02-10 17:02:54 +0100

    Added documentation to class and did some minor refactoring.

--- src/main/java/CapitalizeTextCommand.java
+++ src/main/java/CapitalizeTextCommand.java
@@ -1,7 +1,24 @@
-public class CapitalizeTextCommand implements TextCommand{
+/**
+ * The {@code CapitalizeTextCommand} class implements the {@code TextCommand} interface
+ * and provides functionality to capitalize the first letter of a string.
+ *
+ * @author jonastomren
+ * @version 1.0.0
+ * @since 1.0
+ */
+public class CapitalizeTextCommand implements TextCommand {
+  /**
+   * Executes the capitalization operation, transforming the first letter of the first word in the text
+   * to uppercase while keeping the remaining characters unchanged.
+   *
+   * @param text the input text to be processed.
+   * @return a new string with the first word capitalized.
+   * @throws IllegalArgumentException if the input text is null, empty, blank,
+   * or does not contain the target string.
+   */
   @Override
   public String execute(String text) {
-    if (text == null) {
+    if (text == null || text.isBlank() || text.isEmpty()) {
       throw new IllegalArgumentException();
     }
     return text.substring(0,1).toUpperCase() + text.substring(1);
