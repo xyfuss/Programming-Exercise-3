dd5eb6e1bdc135258f2fe004f4e3db3803c6e98c
xyfuss <jonasaatomren@gmail.com>
2025-02-10 15:21:47 +0100

    added @Override

--- src/main/java/ReplaceFirstTextCommand.java
+++ src/main/java/ReplaceFirstTextCommand.java
@@ -3,7 +3,7 @@ public class ReplaceFirstTextCommand extends ReplaceTextCommand{
   public ReplaceFirstTextCommand(String target, String replacement) {
     super(target,replacement);
   }
-
+  @Override
   public String execute(String text) {
     return text.replaceFirst(this.getTarget(),this.getReplacement());
   }
