a84817518fad9ee6641525d3f063d509b3a946a7
xyfuss <jonasaatomren@gmail.com>
2025-02-10 14:47:16 +0100

    added ReplaceText classes and added initial methods

---
+++ src/main/java/ReplaceTextCommand.java
@@ -0,0 +1,40 @@
+public class ReplaceTextCommand implements TextCommand{
+
+  private String target;
+  private String replacement;
+
+  public ReplaceTextCommand(String target, String replacement) {
+    this.setReplacement(replacement);
+    this.setTarget(target);
+  }
+
+  public String getTarget() {
+    return target;
+  }
+
+  public void setTarget(String target) {
+    if (target == null || target.isBlank() || target.isEmpty()) {
+      throw new IllegalArgumentException();
+    }
+    this.target = target;
+  }
+
+  public String getReplacement() {
+    return replacement;
+  }
+
+  public void setReplacement(String replacement) {
+    if (replacement == null || replacement.isBlank() || replacement.isEmpty()) {
+      throw new IllegalArgumentException();
+    }
+    this.replacement = replacement;
+  }
+
+  public String execute(String text) {
+    return text.replace(this.getTarget(),this.getReplacement());
+  }
+
+
+
+
+}
