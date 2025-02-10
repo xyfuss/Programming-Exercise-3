9a23e5ce1d43c1069aa05b5d3915fdd6bfdaf26c
xyfuss <jonasaatomren@gmail.com>
2025-02-10 16:50:21 +0100

    Added documentation to class.

--- src/main/java/TextCommand.java
+++ src/main/java/TextCommand.java
@@ -1,4 +1,18 @@
-public interface TextCommand {
-  String execute(String text);
+/**
+ * The {@code TextCommand} interface defines the
+ * {@code execute} method for implementing classes to use.
+ *
+ * @author jonastomren
+ * @version 1.0.0
+ * @since 1.0
+ */
 
+public interface TextCommand {
+  /**
+   * Executes a text transformation on the given input.
+   *
+   * @param text the input text to be processed.
+   * @return the transformed text.
+   */
+  String execute(String text);
 }
