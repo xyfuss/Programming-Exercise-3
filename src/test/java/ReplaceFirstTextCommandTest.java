4b99416366f1e90cfe20812bf48a502e9b4f0569
xyfuss <jonasaatomren@gmail.com>
2025-02-10 14:59:38 +0100

    added test case for execute method for ReplaceFirstTextCommandTest class

---
+++ src/test/java/ReplaceFirstTextCommandTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReplaceFirstTextCommandTest {
+
+  @Test
+  void testExecutePositive() {
+    ReplaceFirstTextCommand test1 = new ReplaceFirstTextCommand("target","replacement");
+    assertEquals("text with replacement and target",test1.execute("text with target and target"));
+  }
+}
